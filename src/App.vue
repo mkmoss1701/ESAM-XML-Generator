54726edb51033279c8cc7639dfda06358237990a
jeremiahbratton <jeremiahbratton@me.com>
2024-08-06 21:17:09 +0000

    Adding segment type ID options. Creating segment pair return function

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { ref, computed, reactive } from 'vue';
-import XmlViewer from 'vue3-xml-viewer';
+import { ref, computed, reactive } from "vue";
+import XmlViewer from "vue3-xml-viewer";
+import SegmentTypeId from "./components/SegmentTypeId.vue";
 
+const aquisitionPointIdentity = ref();
 const segmentSource = [];
 const segments = reactive(segmentSource);
+
+/**
+ * Call addSegment to push in a default blank value to get the output rolling.
+ */
 addSegment();
 
+/**
+ * Using a computed property to consistently rebuild the XML output as
+ * the segments object and other options update.
+ *
+ * This looks a bit messy, but for now we need to concat bit of the XML into a single
+ * stirng to feed it into the viewer component.
+ */
 const signalXML = computed(() => {
   const xmlHead = `<SignalProcessingNotification xmlns="urn:cablelabs:iptvservices:esam:xsd:signal:1" xmlns:sig="urn:cablelabs:md:xsd:signaling:3.0" xmlns:common="urn:cablelabs:iptvservices:esam:xsd:common:1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" acquisitionPointIdentity="ExampleESAM">
 
@@ -15,23 +28,49 @@ const signalXML = computed(() => {
 
   const xmlEnd = `</SignalProcessingNotification>`;
   let segmentsOutput = ``;
-  
-  segments.forEach((segment, index) => {
-    segmentsOutput = segmentsOutput.concat(
-    `<ResponseSignal acquisitionPointIdentity="ExampleESAM" acquisitionSignalID="1" signalPointID="${segment.segmentStart}" action="create">
-      <sig:NPTPoint nptPoint="${segment.segmentStart}"/>
-      <sig:SCTE35PointDescriptor spliceCommandType="06">
-        <sig:SegmentationDescriptorInfo segmentEventId="1" segmentTypeId="52" upidType="9" upid="1" duration="PT${segment.segmentDuration}S" segmentNumber="${index}" segmentsExpected="1"/>
-      </sig:SCTE35PointDescriptor>
-    </ResponseSignal>
-    <ConditioningInfo acquisitionSignalIDRef="1" startOffset="PT60S" duration="PT${segment.segmentDuration}S"/>`);
-  }); 
 
-return `${xmlHead}${segmentsOutput}${xmlEnd}`;
+  segments.forEach((segment, index) => {
+    segmentsOutput = segmentsOutput.concat(getSegmentPair(segment, index));
+  });
+
+  return `${xmlHead}${segmentsOutput}${xmlEnd}`;
 });
 
+/**
+ * Lets break this up a little bit to make it easier to read later...
+ */
+function getSegmentPair(segment, index) {
+  //Lets destructure out the pieces of a segment because we have some calculations to make, and its easier to read.
+  const { segmentStart, segmentDuration, segmentTypeID } = segment;
+
+  //Calculate the end of the ending segment
+  const segmentEnd = segmentStart + segmentDuration;
+
+  return `<ResponseSignal acquisitionPointIdentity="${
+    aquisitionPointIdentity.value
+  }" acquisitionSignalID="1" signalPointID="${segmentStart}" action="create">
+      <sig:NPTPoint nptPoint="${segmentStart}"/>
+      <sig:SCTE35PointDescriptor spliceCommandType="06">
+        <sig:SegmentationDescriptorInfo segmentEventId="${index}" segmentTypeId="${segmentTypeId}" upidType="9" upid="1" duration="PT${segmentDuration}S" segmentNumber="${index}" segmentsExpected="1"/>
+      </sig:SCTE35PointDescriptor>
+    </ResponseSignal>
+    <ConditioningInfo acquisitionSignalIDRef="1" startOffset="PT${segmentStart}S" duration="PT${segmentDuration}S"/>
+    
+    <ResponseSignal acquisitionPointIdentity="${
+      aquisitionPointIdentity.value
+    }" acquisitionSignalID="1" signalPointID="${segmentEnd}" action="create">
+      <sig:NPTPoint nptPoint="${segmentEnd}"/>
+      <sig:SCTE35PointDescriptor spliceCommandType="06">
+        <sig:SegmentationDescriptorInfo segmentEventId="1" segmentTypeId="${
+          parseInt(segmentTypeId) + 1
+        }" upidType="9" upid="1" duration="PT${segmentDuration}S" segmentNumber="${index}" segmentsExpected="1"/>
+      </sig:SCTE35PointDescriptor>
+    </ResponseSignal>
+    <ConditioningInfo acquisitionSignalIDRef="1" startOffset="PT60S" duration="PT${segmentDuration}S"/>`;
+}
+
 function addSegment() {
-  segments.push({segmentStart:'', segmentDuration:''});
+  segments.push({ segmentTypeId: 52, segmentStart: "", segmentDuration: "" });
 }
 
 function removeSegment(index) {
@@ -40,27 +79,43 @@ function removeSegment(index) {
 </script>
 
 <template>
-  <main style="display:grid; grid-template-columns: 1fr 1fr; gap:2em;">
+  <main style="display: grid; grid-template-columns: 1fr 1fr; gap: 2em">
     <section>
-        <div v-for="(segment, index) in segments">
-          <pre>{{segments.value}}</pre>
-            <div>
-                <label>Segment Start {{ index + 1 }}</label>
-                <input type="number" name="segmentStart" v-model="segments[index].segmentStart"/>
-            </div>
-            <div>
-                <label>Segment Duration {{ index +1 }}</label>
-                <input type="number" name="segmentDuration" v-model="segments[index].segmentDuration"/>
-            </div>
-            <button @click.prevent="removeSegment(index)">Delete</button>
+      <div>
+        <label>Aquisition Point Identity</label>
+        <input type="text" v-model="aquisitionPointIdentity" />
+      </div>
+
+      <div v-for="(segment, index) in segments">
+        <div>
+          <label>Segment Type ID</label>
+          <segmentTypeId v-model="segments[index].segmentTypeId" />
         </div>
+        <div>
+          <label>Segment Start {{ index + 1 }}</label>
+          <input
+            type="number"
+            name="segmentStart"
+            v-model="segments[index].segmentStart"
+          />
+        </div>
+        <div>
+          <label>Segment Duration {{ index + 1 }}</label>
+          <input
+            type="number"
+            name="segmentDuration"
+            v-model="segments[index].segmentDuration"
+          />
+        </div>
+        <button @click.prevent="removeSegment(index)">Delete</button>
+      </div>
 
-        <button @click="addSegment">Add Segment</button>
-        </section>
+      <button @click="addSegment">Add Segment</button>
+    </section>
 
-        <section>
-          <XmlViewer :xml="signalXML" />
-        </section>
+    <section>
+      <XmlViewer :xml="signalXML" />
+    </section>
   </main>
 </template>
 
---
+++ src/components/SegmentTypeId.vue
@@ -0,0 +1,37 @@
+<script setup>
+const input = defineModel();
+</script>
+
+<template>
+  <select v-model="input">
+    <option value="0">0: Not Indicated</option>
+    <option value="1">1: Content Identification</option>
+    <option value="16">16: Program Start</option>
+    <option value="18">18: Program Early Termination</option>
+    <option value="19">19: Program Breakaway</option>
+    <option value="20">20: Program Resumption</option>
+    <option value="21">21: Program Runover: Planned</option>
+    <option value="22">22: Program Runover: Unplanned</option>
+    <option value="23">23: Program Overlap: Start</option>
+    <option value="24">24: Program Blackout Override</option>
+    <option value="25">25: Program Join</option>
+    <option value="26">26: Program Immediate Resumption</option>
+    <option value="32">32: Chapter Start</option>
+    <option value="34">34: Break Start</option>
+    <option value="48">48: Provider Advertisement Start</option>
+    <option value="50">50: Distributor Advertisement Start</option>
+    <option value="52">52: Provider Placement Opportunity Start</option>
+    <option value="54">54: Distributor Placement Opportunity Start</option>
+    <option value="56">56: Provider Overlay Placement Opportunity Start</option>
+    <option value="58">
+      58: Distributor Overlay Placement Opportunity Start
+    </option>
+    <option value="60">60: Provider Promo</option>
+    <option value="62">62: Distributor Promo Start</option>
+    <option value="64">64: Unscheduled Event</option>
+    <option value="66">66: Alternate Content Opportunity Start</option>
+    <option value="68">68: Provider Ad Block</option>
+    <option value="70">70: Distributor Ad Block</option>
+    <option value="80">80: Network Start</option>
+  </select>
+</template>
